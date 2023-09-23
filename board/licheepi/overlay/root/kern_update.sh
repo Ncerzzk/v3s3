916a8f553d92ad11801f734a02403459d90a870e
ncer <huangcmzzk@gmail.com>
2023-09-23 12:52:36 +0800

    add pwm support. add usb1.1 support
    Signed-off-by: ncer <huangcmzzk@gmail.com>

--- board/licheepi/overlay/root/kern_update.sh
+++ board/licheepi/overlay/root/kern_update.sh
@@ -4,13 +4,14 @@
 # for development purposes
 
 # replace with values appropriate for your setup
-port=2222
-host=richard@192.168.123.11
-zImage_path='~/outbu/zImage'
+host=ncer@192.168.2.105
+zImage_path='~/sata/buildroot-2022.11.1/output/images'
 
 mkdir -p /boot
 mount /dev/mmcblk0p1 /boot
-scp -P $port ${host}:${zImage_path} /boot
+scp ${host}:${zImage_path}/sun8i-v3s-licheepi-zero-dock.dtb /boot/
+scp ${host}:${zImage_path}/zImage /boot/
+scp ${host}:${zImage_path}/rootfs.tar /root/
 umount /boot
 rmdir /boot
 
